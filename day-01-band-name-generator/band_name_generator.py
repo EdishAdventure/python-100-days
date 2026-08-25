ac1cee2a55c94c9d339b82aedbd4e46a55f36623
EdwardShi <140990700+EdishAdventure@users.noreply.github.com>
2026-08-25 19:16:30 -0400

    Complete Day 1 Python exercises

---
+++ day-01-band-name-generator/band_name_generator.py
@@ -0,0 +1,6 @@
+# Topics: string; variables.
+
+print("Welcome to the Band Name Generator.")
+city = input("What city did you grow up in? ")
+pet = input("What is the name of a pet? ")
+print("Your band name could be " + city + " " + pet)
