8b8f3adce5c6bca97553042a6f781631923331dd
EdwardShi <140990700+EdishAdventure@users.noreply.github.com>
2026-08-26 00:12:03 -0400

    complete day 2 calculator - numbers, fstrings, datatypes, conversion

---
+++ day-02-tip-calculator/tip_calculator.py
@@ -0,0 +1,11 @@
+#Topics: numbers, fstrings, datatypes, conversion.
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip_percent = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+tip = bill * (tip_percent / 100)
+total_bill = bill + tip
+bill_per_person = total_bill / people
+final_amount= round(bill_per_person,2)
+print(f"Each person should pay ${final_amount}")
